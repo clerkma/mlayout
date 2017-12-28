0876b5142f378f2d3bf0de204cf2e86c570db619
Clerk Ma <clerkma@gmail.com>
2017-12-28 17:23:50 +0800

    added endian related source.

---
+++ src/ot-bswap.h
@@ -0,0 +1,9 @@
+
+uint16_t ot_bswap16 (uint16_t s);
+uint32_t ot_bswap32 (uint32_t s);
+uint64_t ot_bswap64 (uint64_t s);
+
+void ot_mute_bswap16 (uint8_t * s);
+void ot_mute_bswap24 (uint8_t * s);
+void ot_mute_bswap32 (uint8_t * s);
+void ot_mute_bswap64 (uint8_t * s);
---
+++ src/ot-stream-bswap.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint16_t ot_bswap16 (uint16_t s)
+{
+}
+
+void ot_mute_bswap16 (uint8_t * s)
+{
+}
+
+void ot_mute_bswap24 (uint8_t * s)
+{
+}
+
+uint32_t ot_bswap32 (uint32_t s)
+{
+}
+
+void ot_mute_bswap32 (uint8_t * s)
+{
+}
+
+uint64_t ot_bswap64 (uint64_t s)
+{
+}
+
+void ot_mute_bswap64 (uint8_t * s)
+{
+}
+
